0234e9fdb8a4e0f0c13fdcf77e678c4eea07e98b
Your Name <arpitpandey1439@gmail.com>
2026-06-14 14:56:48 +0530

    fix: remove double api from baseurl

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://adyant-travells.onrender.com/api';
+axios.defaults.baseURL = 'https://adyant-travells.onrender.com';
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
